5a08be4f2f7acd45eca1dd7894a40f38e84cf2af
devin <166536125+Devinthedude515@users.noreply.github.com>
2025-11-01 13:03:32 -0400

    Started JSON and HTML JS files

--- itis3135/scripts/generate_json.js
+++ itis3135/scripts/generate_json.js
@@ -1,58 +1,22 @@
 const generateJSONBtn = document.getElementById("generateJSON");
 
 generateJSONBtn.addEventListener("click", () => {
-
- const introData = {
+    const text = `
+    {
         "firstName": "Devin",
         "prefName": "none",
         "middleInitial": "G",
         "lastName": "Williams",
         "mascotSymbol": "|",
         "charMascot": "Deceptive",
-        "animalMascot": "Wolverine",
-        "imgUpload": "images/Headshot_resized.jpg",
-        "imgCaption": "A professional Photo",
-        "personalStatement": "Hello everyone, my name is Devin Williams and I am originally from Cincinnati, OH. I have been living in Charlotte since I was ten years old in 2009. I am into Sci-fi/fantasy media meaning anything surrounding games, books, movies, graphic novels, and t.v. shows. I also cosplay and go to events from time to time. I look forward to meeting everyone during this semester.",
-        "perBackground": "Not too different from my personal statement. The only thing I can add is that I have been married for seven years.",
-        "profBackground": "I unfortunately have no professional computer related experience. My only professional experience in my adult life is serving in the U.S. Army.",
-        "PC": "I have primarily used Windows OS for a number of years. I mostly use my desktop at home for any computer needs.",
-        "coursesTaking": [
-            {
-            "course": "ITIS 3160 - Database Design",
-            "reason": "To understand the basics of storing data."
-            },
-            {
-            "course": "ITIS 3146 - Intro Oper Syst & Networking",
-            "reason": "To get more infomation on how operating systems and networks function."
-            },
-            {
-            "course": "ITIS 3155 - Software Engineering",
-            "reason": "To understand the basics of the Industry."
-            }
-        ],
-        "quote": "In a world filled with misery and uncertainty, it is a great comfort to know that in the end, there is light in the darkness",
-        "author": "Joshua Graham",
-        "funOrInt": "I am huge fan of the Fallout Franchise from the games to the T.V. show.",
-        "share": "none",
-        "links": [
-                {
-                "name": "GitHub",
-                "href": "https://github.com/devinthedude515"
-                },
-                {
-                "name": "GitHub Page",
-                "href": "https://github.com/Devinthedude515/Devinthedude515.github.io"
-                },
-                {
-                "name": "freeCodeCamp",
-                "href": "https://www.freecodecamp.org/fcc-2cfb918d-d7d3-4b1f-b356-76f5751d3961"
-                },
-                {
-                "name": "LinkedIn",
-                "href": "https://www.linkedin.com/in/devin-williams-8b6a36197/"
-                }
-            ]
-    };
-
-    
+        "animalMascot": "Pegasus",
+        "imgUpload": "images/Headshot_resized.jpeg",
+        "imgCaption": "At the beach on the eastern coast of Florida (2024)",
+        "perStatement": "...",
+        "perBackground": "Grew up north of Charlotte, and have always had a love of computers.",
+        "profBackground": "This is my first semester as an Instructional Assistant/Teachers Assistant, but before that I was a Peer Tutor for CCI.",
+        "academicBackground": "I’m currently a Junior at UNC Charlotte studying computer science with a focus in Cybersecurity. Before that I attended Highschool in Mooresville, North Carolina.",
+        "subjectBackground": "...",
+        "PC": "The laptop I use for university is a Macbook Pro M2 14 inch. I also use a custom built Windows 11 computer."}
+    `;
 });
